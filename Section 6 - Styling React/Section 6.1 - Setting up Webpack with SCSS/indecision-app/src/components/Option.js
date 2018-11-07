5cd06d2f836f042a0c8f3158bb89ca8e1dc4f30c
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-11-07 23:08:24 +0000

    Section 6 - Styling React - 6.1. Setting up Webpack with SCSS.

---
+++ Section 6 - Styling React/Section 6.1 - Setting up Webpack with SCSS/indecision-app/src/components/Option.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Option = (props) => (
+   <div>
+      {props.optionText}
+      <button onClick={ (e) => {props.handleDeleteOption(props.optionText)} }>
+         Remove
+      </button>
+   </div>
+);
+
+export default Option;
+
+
+// Implications on React DevTool if we were to use the inline export default = (props) => {...}
+// We cannot use export default on variables but if we were to remove the variable name and export default the function - the component would work normally but this will display the component within the React DevTool as unknown. Therefore it is better to use export default Option variant rather than the inline for this very reason.
