315e59649068d6f3482cc70a559531e7ab7e02bd
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-11-08 21:06:58 +0000

    Section 6 - Styling React - 6.2. Architecture and Header Styles.

---
+++ Section 6 - Styling React/Section 6.2 - Architecture and Header Styles/indecision-app/webpack.config.js
@@ -0,0 +1,35 @@
+// Documentation: https://webpack.js.org/
+// Node Path API: https://nodejs.org/api/path.html
+const path = require('path');
+
+// entry point -> output
+module.exports = {
+   entry: './src/app.js',
+   output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [{
+         loader: 'babel-loader',
+         test: /\.js$/,
+         exclude: /node_modules/
+      }, {
+         test: /\.scss$/,
+         use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+         ]
+      }]
+   },
+   devtool: 'cheap-module-eval-source-map',
+   devServer: {
+      contentBase: path.join(__dirname, 'public')
+   }
+};
+
+
+// __direname Provides the absolute location path for the current location.
+// path.join method allows you to join directory paths to give a final path yuo want.
+// console.log(path.join(__dirname, 'public'));
