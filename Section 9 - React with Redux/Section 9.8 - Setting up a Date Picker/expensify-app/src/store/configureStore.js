adb6546fc8d773377bdcd046f58ea3bd0d473b9b
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-11-23 23:36:02 +0000

    Section 9 - React with Redux - 9.8. Setting up a Date Picker.

---
+++ Section 9 - React with Redux/Section 9.8 - Setting up a Date Picker/expensify-app/src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses'
+import filtersReducer from '../reducers/filters'
+
+
+//--------------------------------
+// Create Redux Store:
+//--------------------------------
+export default () => {
+   const store = createStore(
+      combineReducers({
+         expenses: expensesReducer,
+         filters: filtersReducer
+      })
+   );
+
+   return store;
+};
