632c3f0c8893624fea694ae0d4e5cba7040178f6
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-11-24 22:40:57 +0000

    Section 10 - Testing Your Application - 10.1. Setting up Jest.

---
+++ Section 10 - Testing Your Application/Section 10.1 - Setting up Jest/expensify-app/src/components/ExpenseList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseListItem from './ExpesneListItem';
+import selectExpenses from '../selectors/expenses';
+
+
+const ExpenseList = (props) => (
+   <div>
+      <h1>ExpenseList</h1>
+      {props.expenses.map((expense) => {
+         return <ExpenseListItem key={expense.id} {...expense} />
+      })}
+   </div>
+);
+
+const mapStateToProps = (state) => {
+   return {
+      expenses: selectExpenses(state.expenses, state.filters)
+   };
+};
+
+export default connect(mapStateToProps)(ExpenseList);
