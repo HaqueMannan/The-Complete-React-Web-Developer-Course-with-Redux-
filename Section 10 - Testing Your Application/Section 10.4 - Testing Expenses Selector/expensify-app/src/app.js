124d49aae9f1072e304502bdb46d95b9b2ec1d3a
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-11-25 00:48:28 +0000

    Section 10 - Testing Your Application - 10.4. Testing Expenses Selector.

---
+++ Section 10 - Testing Your Application/Section 10.4 - Testing Expenses Selector/expensify-app/src/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routes/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+// create Redux Store:
+const store = configureStore();
+
+// Dispatch Actions:
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 65000 }));
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+   <Provider store={store}>
+      <AppRouter />
+   </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
