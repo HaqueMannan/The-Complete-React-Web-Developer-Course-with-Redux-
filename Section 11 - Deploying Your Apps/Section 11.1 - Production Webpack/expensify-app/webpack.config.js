9b83a4ad14bc1630d5cff0d6956c7be0a3e02fab
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-02 13:09:49 +0000

    Section 11 - Deploying Your Apps - 11.1. Production Webpack.

---
+++ Section 11 - Deploying Your Apps/Section 11.1 - Production Webpack/expensify-app/webpack.config.js
@@ -0,0 +1,39 @@
+// Documentation: https://webpack.js.org/
+// Node Path API: https://nodejs.org/api/path.html
+const path = require('path');
+
+// entry point -> output
+module.exports = (env) => {
+   const isProduction = env === 'production';
+
+   return {
+      entry: './src/app.js',
+      output: {
+         path: path.join(__dirname, 'public'),
+         filename: 'bundle.js'
+      },
+      module: {
+         rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+         }, {
+            test: /\.s?css$/,
+            use: [
+               'style-loader',
+               'css-loader',
+               'sass-loader'
+            ]
+         }]
+      },
+      devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+      devServer: {
+         contentBase: path.join(__dirname, 'public'),
+         historyApiFallback: true
+      }
+   };
+};
+
+// __direname Provides the absolute location path for the current location.
+// path.join method allows you to join directory paths to give a final path yuo want.
+// console.log(path.join(__dirname, 'public'));
