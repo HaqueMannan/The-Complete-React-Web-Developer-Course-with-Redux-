20e9cc5b03148c52ddad07c898ef1fcb31a62c7e
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-14 17:32:23 +0000

    Section 13 - Firebase with Redux - 13.1. Asynchronous Redux Actions.

---
+++ Section 13 - Firebase with Redux/Section 13.1 - Asynchronous Redux Actions/expensify-app/src/components/AddExpensePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import { connect } from 'react-redux';
+import { startAddExpense } from '../actions/expenses';
+
+
+export class AddExpensePage extends React.Component {
+   onSubmit = (expense) => {
+      this.props.startAddExpense(expense);
+      this.props.history.push('/');
+   };
+   render() {
+      return (
+         <div>
+            <h1>Add Expense</h1>
+            <ExpenseForm 
+               onSubmit={this.onSubmit}
+            />
+         </div>
+      );
+   };
+};
+
+const mapDispatchToProps = (dispatch) => ({ 
+   startAddExpense: (expense) => dispatch(startAddExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
