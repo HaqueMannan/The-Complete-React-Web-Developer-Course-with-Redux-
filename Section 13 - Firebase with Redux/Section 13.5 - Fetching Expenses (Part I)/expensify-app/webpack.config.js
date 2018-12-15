5bfbc73ed4ab1421395f5b4fc7c51cf4759c0a69
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-15 17:28:57 +0000

    Section 13 - Firebase with Redux - 13.5. Fetching Expenses: Part II.

--- Section 13 - Firebase with Redux/Section 13.5 - Fetching Expenses (Part I)/expensify-app/webpack.config.js
+++ Section 13 - Firebase with Redux/Section 13.5 - Fetching Expenses (Part I)/expensify-app/webpack.config.js
@@ -9,9 +9,9 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // Environment variable - stores environment currently in:
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
    require('dotenv').config({ path: '.env.test' });
-} else if (process.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'development') {
    require('dotenv').config({ path: '.env.development' });
 }
 
