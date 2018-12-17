d20f54a663817c8327481ebf5683b1abad6abff3
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-17 23:26:44 +0000

    Section 14 - Firebase Authentication - 14.4. The Auth Reducer.

---
+++ Section 14 - Firebase Authentication/Section 14.4 - The Auth Reducer/expensify-app/src/routers/AppRouter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import LoginPage from '../components/LoginPage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
+
+
+export const history = createHistory();
+
+const AppRouter = () => (
+   <Router history={history}>
+      <div>
+         <Header />
+         <Switch>
+            <Route path='/' component={LoginPage} exact={true} />
+            <Route path='/dashboard' component={ExpenseDashboardPage} />
+            <Route path='/create' component={AddExpensePage} />
+            <Route path='/edit/:id' component={EditExpensePage} />
+            <Route path='/help' component={HelpPage} />
+            <Route component={NotFoundPage} />
+         </Switch>
+      </div>
+   </Router>
+);
+
+export default AppRouter;
